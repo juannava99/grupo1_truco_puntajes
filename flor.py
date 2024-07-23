9afa6220faaac2753725178145e923d74624dcd5
FedeOrtiz <federicoortiz185@gmail.com>
2024-07-23 14:39:42 -0300

    actualizacion funciones dentro de flor.py

--- flor.py
+++ flor.py
@@ -1,8 +1,21 @@
-
-#Función para preguntar si hubo flor en la ronda.
 def preguntar_flor():
-    pass
+    flor = input("Hay flor en esta mano? (s/n): ")
+    return flor.lower() == 's'
 
-#Función para actualizar el puntaje en caso de que haya flor.
-def gestionar_flor(puntaje_equipo_1, puntaje_equipo_2):
-    pass
+
+def gestionar_flor(pts_equipo_1, pts_equipo_2):
+    pts = 3
+    while True:
+        ganador = input("¿Quién ganó la flor? (e1 o e2): ").lower()
+        if ganador in ['e1', 'e2']:
+            if ganador == 'e1':
+                pts_equipo_1 += pts
+                print(f"Equipo 1 gana {pts} puntos por la flor.")
+            elif ganador == 'e2':
+                pts_equipo_2 += pts
+                print(f"Equipo 2 gana {pts} puntos por la flor.")
+            break
+        else:
+            print("Introduzca un valor válido (e1 o e2)")
+    
+    return pts_equipo_1, pts_equipo_2
