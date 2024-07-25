63d1fdb3def74e0d3d72414db1bdf1b9ba003946
Rony <ronilunda559@gmail.com>
2024-07-25 01:22:53 -0300

    verifica muestra puntos

--- puntaje.py
+++ puntaje.py
@@ -1,8 +1,13 @@
 
 #Función para verificar si unequipo ha ganado.
 def verificar_victoria(puntaje):
-    pass 
+    if puntaje <= 30:
+        return True
+    else:
+        return False
+
 
 #Función para mostrar los puntajes actuales.
 def mostrar_puntajes(puntaje_equipo_1,puntaje_equipo_2):
-    pass 
+    print(f"El puntaje del equipo 1 Es: {puntaje_equipo_1}")
+    print(f"El puntaje del equipo 2 Es: {puntaje_equipo_2}")
