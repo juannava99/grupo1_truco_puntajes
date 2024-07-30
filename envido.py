588c729f2afd5c64d7b8a954d9dc5316719492bf
MarioMilito <75212047+soymariomilito@users.noreply.github.com>
2024-07-29 22:22:52 -0300

    Version probada del envido.py

--- envido.py
+++ envido.py
@@ -1,14 +1,12 @@
-####Fede, ahi subi el modulo truco, en relacion a este modulo entiendo que la funcion preguntar_envido y puntos_envido, en caso de envido envido no querido sumaria 1 puntos, y deberian ser 3
-
 # Función para preguntar el tipo de envido.
 def preguntar_envido():
     x = input("¿Se jugó el envido? (s/n): ").lower()
     if x == 'n':
         return False, None
     elif x == 's':
-        opciones_validas = ["envido", "real envido", "envido envido", "falta envido", "no querido"]
+        opciones_validas = ["1", "2", "3", "4", "5"]
         while True:
-            opcion = input("¿Qué tipo de envido se jugó? (envido/real envido/envido envido/falta envido/no querido): ").lower()
+            opcion = input("¿Qué tipo de envido se jugó?\n1.envido\n2.real envido\n3.envido envido\n4.falta envido\n5.no querido\nIngrese la opcion: ").lower()
             if opcion in opciones_validas:
                 return True, opcion
             else:
@@ -19,21 +17,21 @@ def preguntar_envido():
 
 #Función para calcular los puntos del envido.
 def puntos_envido(opcion, puntaje_ganador,puntaje_perdedor):
-    if opcion == 'envido':
+    if opcion == "1":
         return 2 
-    elif opcion == 'real envido':
+    elif opcion == "2":
         return 3
-    elif opcion == 'envido envido':
+    elif opcion == "3":
         return 4
     
     #creo que la de falta envido esta mal, habria que chequear
     
-    elif opcion == 'falta envido':
-        if puntaje_perdedor < 15: 
+    elif opcion == "4":
+        if puntaje_perdedor <= 15: 
             return 30 - puntaje_ganador
         else:
             return 30 - puntaje_perdedor
-    elif opcion == 'no querido':
+    elif opcion == "5":
         return 1 
     return 0 # en caso de que no coincida con ninguna 
 
@@ -42,38 +40,17 @@ def gestionar_envido(puntaje_equipo_1,puntaje_equipo_2):
     se_jugo,opcion = preguntar_envido()
     if se_jugo:
         puntos = puntos_envido(opcion,puntaje_equipo_1,puntaje_equipo_2)
-        ganador = input("Quien gano el envido? (e1 o e2): ").lower()
-        if ganador == 'e1':
+        ganador = input("Quien gano el envido? (1/2): ")
+        if ganador == '1':
+            print(f"Se anotaran {puntos} puntos al equipo {ganador}\n")
             puntaje_equipo_1 += puntos
-        elif ganador == 'e2':
+        elif ganador == '2':
+            print(f"Se anotaran {puntos} puntos al equipo {ganador}\n")
             puntaje_equipo_2 += puntos
         else:
-            print("Opcion no valida, ingrese 'e1' o 'e2'.")
+            print("Opcion no valida, ingrese '1' o '2'.")
             return gestionar_envido(puntaje_equipo_1,puntaje_equipo_2) # en caso de que cargue una opcion no valida vuelve a preguntar
     else:
         print("No se jugo el envido en esta mano.")
         return puntaje_equipo_1, puntaje_equipo_2,False
     return puntaje_equipo_1, puntaje_equipo_2,True
-
-
-#def main():
-#    puntaje_equipo_1 = 0
-#    puntaje_equipo_2 = 0
-#    while True:
-#        print("Puntaje actual:")
-#        print(f"Equipo 1: {puntaje_equipo_1}")
-#        print(f"Equipo 2: {puntaje_equipo_2}")
-#        print("¿Qué deseas hacer?")
-#        print("1. Anotar envido")
-#        ###print (Anotar envido)
-#        ###print (Anotar irse al mazo)
-#        print("2. Salir")
-#        opcion = input("Ingrese una opción: ")
-#        if opcion == "1":
-#            puntaje_equipo_1, puntaje_equipo_2 = gestionar_envido(puntaje_equipo_1, puntaje_equipo_2)
-#        elif opcion == "2":
-#            break
-#        else:
-#            print("Opción inválida. Por favor, ingrese una opción válida.")
-#
-#main()
