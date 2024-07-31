4b3350461f05ff291693c64e6d4d60b516b1888f
soymariomilito <mariomilitotrade@gmail.com>
2024-07-31 20:16:37 -0300

    Agregamos control de errores al modulo del truco

--- truco.py
+++ truco.py
@@ -1,12 +1,15 @@
 def preguntar_truco():
-    print("¿Qué tipo de truco se jugó?")
-    print("1. Truco")
-    print("2. Truco, Quiero Retruco")
-    print("3. Truco, Quiero Retruco y Quiero Vale 4")
-    print("4. No se cantó truco")
-    opcion = input("Ingrese una opción: ")
-    return opcion
-
+    while True:
+        print("¿Qué tipo de truco se jugó?")
+        print("1. Truco")
+        print("2. Truco, Quiero Retruco")
+        print("3. Truco, Quiero Retruco y Quiero Vale 4")
+        print("4. No se cantó truco")
+        opcion = input("Ingrese una opción: ")
+        if opcion in ["1", "2", "3", "4"]:
+            return opcion
+        else:
+            print("Opción inválida. Por favor, ingrese una opción válida (1-4).")
 
 def puntos_truco(opcion):
     if opcion == "4":
@@ -21,18 +24,23 @@ def puntos_truco(opcion):
         return 4 if querido == "s" else 3
     else:
         print("Opción inválida. No se anotaron puntos.")
-        return 0
+        
 
 
 def gestionar_truco(puntaje_equipo_1, puntaje_equipo_2):
     opcion = preguntar_truco()
     puntos = puntos_truco(opcion)
 
-    if puntos == 0:
-        equipo_ganador = input("¿Qué equipo ganó la mano? (1/2): ")
-        puntos = 1
-    else:
-        equipo_ganador = input("¿Qué equipo ganó el truco? (1/2): ")
+    while True:
+        if puntos == 0:
+            equipo_ganador = input("¿Qué equipo ganó la mano? (1/2): ")
+        else:
+            equipo_ganador = input("¿Qué equipo ganó el truco? (1/2): ")
+        
+        if equipo_ganador in ["1", "2"]:
+            break
+        else:
+            print("Opción inválida. Por favor, ingrese '1' o '2'.")
 
     if equipo_ganador == "1":
         puntaje_equipo_1 += puntos
